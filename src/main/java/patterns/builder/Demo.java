0806b20986fca9583a7aa8647cd049a043d1cbbd
Kirils <kirils.putimcevs@accenture.com>
2021-02-17 23:24:43 +0200

    Add builder pattern

---
+++ src/main/java/patterns/builder/Demo.java
@@ -0,0 +1,10 @@
+package patterns.builder;
+
+public class Demo {
+    public static void main(String ...args) {
+        HtmlBuilder builder = new HtmlBuilder("ul")
+                .addChild("li", "hello")
+                .addChild("li", "world");
+        System.out.println(builder.toString());
+    }
+}
---
+++ src/main/java/patterns/builder/HtmlBuilder.java
@@ -0,0 +1,28 @@
+package patterns.builder;
+
+class HtmlBuilder {
+    private String rootName;
+    private HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(String rootName) {
+        this.rootName = rootName;
+        root.name = rootName;
+    }
+
+    public HtmlBuilder addChild(String childName, String childText) {
+        HtmlElement e = new HtmlElement(childName, childText);
+        root.elements.add(e);
+        return this;
+    }
+
+    public HtmlBuilder clear() {
+        root = new HtmlElement();
+        root.name = rootName;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
+    }
+}
